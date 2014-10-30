b86188d4dcb37a1271607c7b95e46237392c96e6
pierre.gerhard <pierre.gerhard@gmail.com>
2014-10-30 21:22:08 +0100

    Start working on new template for Maxwell_DG

---
+++ maxwell/minmalist_maxwell.hpp
@@ -0,0 +1,31 @@
+/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t -*- vim:fenc=utf-8:ft=tcl:et:sw=4:ts=4:sts=4
+This file is part of the Feel library
+Author(s): Samuel Quinodoz
+Christophe Prud'homme <christophe.prudhomme@feelpp.org>
+Date: 2009-02-25
+Copyright (C) 2007 Samuel Quinodoz
+Copyright (C) 2009 Université Joseph Fourier (Grenoble I)
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3.0 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
+using namespace Feel;
+using namespace Feel::vf;
+
+template<,AB,C>
+class Maxwell_DG : public Application
+{
+typedef Application super;
+public:
+
+private:
+}
